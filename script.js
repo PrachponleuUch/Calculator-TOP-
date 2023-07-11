6ce66d076362f76a32331b3bcfa4feca72c94aab
PrachponleuUch <ponleuvuthy@gmail.com>
2023-07-11 17:19:50 +1000

    Added Multiply, Divide and Equal functionalities

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ function subtract(a, b){
 }
 
 function multiply(a, b){
-  
+  number1 = a * b;
+  number2 = '';
+  output.textContent = a * b;
 }
 
 function divide(a, b){
-  
+  number1 = a / b;
+  number2 = '';
+  output.textContent = a / b;
 }
 
 function operate(a, b, op){
@@ -30,6 +34,30 @@ function operate(a, b, op){
       subtract(a,b);
       op = '';
       break;
+    case "multiply":
+      multiply(a,b);
+      op = '';
+      break;
+    case "divide":
+      divide(a,b);
+      op = '';
+      break;  
+  }
+}
+
+function operatorCheck(){
+  // If an operator has been click once before, do the operation.
+  if(op === equalBtn.value)
+  {
+    output.textContent = number1;
+  }
+  else if(op){
+    operate(number1, number2, op)
+  }
+  // Else push the value from number2 to number1
+  else{
+    number1 = number2;
+    number2 = '';
   }
 }
 
@@ -39,6 +67,9 @@ let output = document.querySelector('h1');
 let reset = document.querySelector('#reset');
 let additionBtn = document.querySelector('#add');
 let subtractBtn = document.querySelector('#subtract');
+let multiplyBtn = document.querySelector('#multiply');
+let divideBtn = document.querySelector('#divide');
+let equalBtn = document.querySelector('#equal');
 let number1 = localStorage.number1 = '';
 let number2 = localStorage.number2 = '';
 let op = localStorage.op = '';
@@ -60,28 +91,30 @@ reset.addEventListener('click', (e) => {
 
 additionBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  // If an operator has been click once before, do the operation.
-  if(op){
-    operate(number1, number2, op)
-  }
-  // Else push the value from number2 to number1
-  else{
-    number1 = number2;
-    number2 = '';
-  }
+  operatorCheck();
   op = additionBtn.value;
 })
 
 subtractBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  // If an operator has been click once before, do the operation.
-  if(op){
-    operate(number1, number2, op)
-  }
-  // Else push the value from number2 to number1
-  else{
-    number1 = number2;
-    number2 = '';
-  }
+  operatorCheck();
   op = subtractBtn.value;
+})
+
+multiplyBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  operatorCheck();
+  op = multiplyBtn.value;
+})
+
+divideBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  operatorCheck();
+  op = divideBtn.value;
+})
+
+equalBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  operatorCheck();
+  op = equalBtn.value;
 })
